057e38f9021e22265d388bec9130409ed359c93b
Garrett Tolbert <garrett1.tolbert@gmail.com>
2024-06-26 21:50:04 -0500

    add redirects/rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/dashboard',
+				permanent: false,
+				has: [
+					{
+						type: 'cookie',
+						key: '__session',
+					},
+				],
+			},
+		];
+	},
+	rewrites() {
+		return [
+			{
+				source: '/home',
+				destination: '/',
+			},
+		];
+	},
+};
 
 export default nextConfig;
