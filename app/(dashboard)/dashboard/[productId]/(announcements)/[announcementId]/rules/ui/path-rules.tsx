6ff07ac7179575e97f596659b3b26cb6550415e9
Garrett Tolbert <garrett1.tolbert@gmail.com>
2024-07-10 23:40:25 -0500

    show info tooltip for blocklist

--- app/(dashboard)/dashboard/[productId]/(announcements)/[announcementId]/rules/ui/path-rules.tsx
+++ app/(dashboard)/dashboard/[productId]/(announcements)/[announcementId]/rules/ui/path-rules.tsx
@@ -10,6 +10,8 @@ import { useParams } from 'next/navigation';
 import { Label } from '@/ui/label';
 import { Input } from '@/ui/input';
 import { RulesKvResponse } from '@/lib/types';
+import { Info } from 'lucide-react';
+import { Tooltip, TooltipTrigger, TooltipContent } from '@/ui/tooltip';
 
 interface Props extends PropsWithChildren {
 	rules: RulesKvResponse['paths'] | null;
@@ -57,6 +59,50 @@ export default function PathRules({ rules, children }: Props) {
 					<input type='hidden' name='announcementId' value={params.announcementId as string} />
 					<div className='space-y-4'>
 						<div className='space-y-1'>
+							<div className='flex items-center gap-1.5'>
+								<Label htmlFor='blocklist' className='text-right'>
+									Blocklist
+								</Label>
+								<Tooltip delayDuration={100}>
+									<TooltipTrigger asChild>
+										<Info size={16} className='text-gray-500' />
+									</TooltipTrigger>
+									<TooltipContent className='max-w-xs'>
+										<p>
+											The blocklist takes precedence over the allowlist. For example,{' '}
+											<span className='inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10'>
+												/blog/drafts/my-blog
+											</span>{' '}
+											will be blocked with the following rules:
+										</p>
+										<div className='mt-4 flex items-center justify-between'>
+											<span className='text-muted-foreground text-xs'>Blocklist</span>
+											<span className='text-muted-foreground text-xs'>Allowlist</span>
+										</div>
+										<div className='flex items-center justify-between'>
+											<span className='inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10'>
+												/blog/drafts/*
+											</span>
+											<span className='inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10'>
+												/blog/*
+											</span>
+										</div>
+									</TooltipContent>
+								</Tooltip>
+							</div>
+							<Input
+								id='blocklist'
+								name='blocklist'
+								value={blocklist}
+								showClearButton={blocklist !== ''}
+								onChange={(e) => setBlocklist(e.target.value)}
+								placeholder='/blog/drafts/*, /products/archived/*'
+							/>
+							<span className='text-xs text-gray-500'>
+								This announcement won&apos;t be shown on these paths
+							</span>
+						</div>
+						<div className='space-y-1'>
 							<Label htmlFor='allowlist' className='text-right'>
 								Allowlist
 							</Label>
@@ -70,22 +116,6 @@ export default function PathRules({ rules, children }: Props) {
 							/>
 							<span className='text-xs text-gray-500'>
 								This announcement will be shown on these paths
-							</span>
-						</div>
-						<div className='space-y-1'>
-							<Label htmlFor='blocklist' className='text-right'>
-								Blocklist
-							</Label>
-							<Input
-								id='blocklist'
-								name='blocklist'
-								value={blocklist}
-								showClearButton={blocklist !== ''}
-								onChange={(e) => setBlocklist(e.target.value)}
-								placeholder='/blog/drafts/*, /products/archived/*'
-							/>
-							<span className='text-xs text-gray-500'>
-								This announcement won&apos;t be shown on these paths
 							</span>
 						</div>
 					</div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { GeistSans } from 'geist/font/sans';
 import ToastWrapper from '@/ui/toast-wrapper';
 import Script from 'next/script';
+import { TooltipProvider } from '@/ui/tooltip';
 
 export const metadata: Metadata = {
 	title: 'Corie',
@@ -20,7 +21,7 @@ export default function RootLayout({
 			<html lang='en'>
 				<body className={`${GeistSans.className} h-[100dvh]`}>
 					<ToastWrapper />
-					{children}
+					<TooltipProvider>{children}</TooltipProvider>
 					<Script
 						async
 						type='module'
