30286b02b1858093cc494bd23adab3cce97606a7
Garrett Tolbert <garrett1.tolbert@gmail.com>
2024-07-18 11:58:16 -0500

    minor change

--- app/(dashboard)/dashboard/[productId]/(announcements)/[announcementId]/analytics/page.tsx
+++ app/(dashboard)/dashboard/[productId]/(announcements)/[announcementId]/analytics/page.tsx
@@ -3,19 +3,21 @@ import ViewsChart from './ui/views-chart';
 import { EngagementScore } from './ui/engagement-score';
 import { CountriesPreview } from './ui/countries-preview';
 
-const fetcher = (url: string) =>
-	fetch(url, { headers: { Authorization: `Bearer ${process.env.TINYBIRD_API_KEY}` } }).then((res) => res.json());
-
 type ViewsRes = { data: { time: string; views: number }[] };
 type EngagementRes = { data: { avgEngagementScore: number; totalInteractions: number }[] };
 type CountriesRes = { data: { country: string; count: number }[] };
 
+const endpoints = ['anl_views', 'anl_engagement', 'anl_countries'];
+
 export default async function AnalyticsPage({ params }: { params: { announcementId: string } }) {
-	const [viewsRes, engagementRes, countriesRes] = (await Promise.all([
-		fetcher(`${TINYBIRD_ANALYTICS_API_URL}/anl_views.json?announcement_id=${params.announcementId}`),
-		fetcher(`${TINYBIRD_ANALYTICS_API_URL}/anl_engagement.json?announcement_id=${params.announcementId}`),
-		fetcher(`${TINYBIRD_ANALYTICS_API_URL}/anl_countries.json?announcement_id=${params.announcementId}`),
-	])) as [ViewsRes, EngagementRes, CountriesRes];
+	const [viewsRes, engagementRes, countriesRes] = (await Promise.all(
+		endpoints.map((endpoint) =>
+			fetch(`${TINYBIRD_ANALYTICS_API_URL}/${endpoint}.json?announcementId=${params.announcementId}`, {
+				headers: { Authorization: `Bearer ${process.env.TINYBIRD_API_KEY}` },
+				cache: 'no-store',
+			}).then((res) => res.json())
+		)
+	)) as [ViewsRes, EngagementRes, CountriesRes];
 
 	return (
 		<div className='mx-auto w-full p-6 grid grid-cols-2 gap-6'>
