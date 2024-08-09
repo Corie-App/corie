35cba54473b16b79ba55c46f31b351ad529e5167
Garrett Tolbert <garrett1.tolbert@gmail.com>
2024-08-09 12:57:26 -0500

    update baseurl

--- platform/core/base-initializer.ts
+++ platform/core/base-initializer.ts
@@ -15,8 +15,8 @@ export abstract class BaseInitializer {
 
 	protected async fetchEnv(): Promise<void> {
 		try {
-			// const baseUrl = 'https://www.corie.io';
-			const baseUrl = 'https://corie-git-gt-codes-cor-25-track-and-retain-dismisses-gt-codes.vercel.app';
+			const baseUrl = 'https://www.corie.io';
+			// const baseUrl = 'https://corie-git-gt-codes-cor-25-track-and-retain-dismisses-gt-codes.vercel.app';
 			// const baseUrl = 'http://localhost:3000';
 
 			const callbackName = `corie_env_${Date.now()}`;
